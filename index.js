de039c9dfa60086273148e001cb3f1d773396df6
Cesar Amado Colina <amadocesar8@gmail.com>
2023-10-28 20:27:55 -0400

    Asignacion 1 completa

--- index.js
+++ index.js
@@ -24,10 +24,19 @@ app.use(express.urlencoded({ limit: '5mb', extended: true}));
 app.get('/texto',(req,res)=>{
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Este es un mensaje de texto.');
+    console.log("GET con texto");
+})
+
+app.get('/json',(req,res)=>{
+    const jsonResponse = { mensaje: 'Este es un mensaje JSON' };
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(jsonResponse));
+    console.log("GET con json");
 })
 
 app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'html','asignacion1.html'));
+    console.log("GET con html");
 })
 
 
